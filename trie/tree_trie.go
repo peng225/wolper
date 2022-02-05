f31d55f48753311e083c40555b7e9dbb1af58263
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-02-05 11:20:52 +0900

    support include parameter.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- trie/tree_trie.go
+++ trie/tree_trie.go
@@ -9,9 +9,25 @@ type TreeTrie struct {
 	child    *TreeTrie
 }
 
+func deleteChar(s []byte, c byte) []byte {
+	foundIndex := -1
+	for i, v := range s {
+		if v == c {
+			foundIndex = i
+			break
+		}
+	}
+	if foundIndex == -1 {
+		return s
+	} else {
+		s[foundIndex] = s[len(s)-1]
+		return s[:len(s)-1]
+	}
+}
+
 func (treeTrie *TreeTrie) Query(key string, include string, exclude string, current string) []string {
 	if key == "" {
-		if treeTrie.terminal {
+		if treeTrie.terminal && include == "" {
 			return []string{current}
 		} else {
 			return make([]string, 0)
@@ -24,13 +40,25 @@ func (treeTrie *TreeTrie) Query(key string, include string, exclude string, curr
 	result := make([]string, 0)
 	for currentNode != nil {
 		if !strings.Contains(exclude, string(currentNode.c)) {
+			newInclude := include
 			if firstChar == '.' {
+				foundInclude := false
+				if strings.Contains(include, string(currentNode.c)) {
+					foundInclude = true
+					newInclude = string(deleteChar([]byte(include), currentNode.c))
+				}
 				current += string(currentNode.c)
-				result = append(result, currentNode.Query(key, include, exclude, current)...)
+				result = append(result, currentNode.Query(key, newInclude, exclude, current)...)
 				current = current[:len(current)-1]
+				if foundInclude {
+					include += string(currentNode.c)
+				}
 			} else if currentNode.c == firstChar {
+				if strings.Contains(include, string(currentNode.c)) {
+					newInclude = string(deleteChar([]byte(include), currentNode.c))
+				}
 				current += string(currentNode.c)
-				result = append(result, currentNode.Query(key, include, exclude, current)...)
+				result = append(result, currentNode.Query(key, newInclude, exclude, current)...)
 				break
 			}
 		}
