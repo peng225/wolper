8c60f0c0c7e6d3926f8434ecd0024176f399e283
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-02-12 12:22:26 +0900

    add client server connection test.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/peng225/wolper
 go 1.17
 
 require (
+	github.com/onsi/ginkgo/v2 v2.1.1
+	github.com/onsi/gomega v1.18.1
 	github.com/spf13/cobra v1.3.0
 	github.com/stretchr/testify v1.7.0
 	google.golang.org/grpc v1.44.0
@@ -19,5 +21,6 @@ require (
 	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220126215142-9970aeb2e350 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ service/service_test.go
@@ -0,0 +1,103 @@
+package service_test
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/peng225/wolper/dictionary"
+	"github.com/peng225/wolper/pb"
+	"github.com/peng225/wolper/service"
+)
+
+func findWord(key string, words []string) bool {
+	for _, word := range words {
+		if key == word {
+			return true
+		}
+	}
+	return false
+}
+
+var _ = Describe("Service", func() {
+	var (
+		dict      dictionary.Dictionary
+		registrar *grpc.Server
+	)
+
+	BeforeEach(func() {
+		dict = dictionary.Dictionary{}
+	})
+
+	AfterEach(func() {
+		registrar.GracefulStop()
+	})
+
+	Describe("Client and server communicate successfully", func() {
+		Context("With proper settings", func() {
+			port := 8080
+			It("should get the correct result", func() {
+				test_dict_name := "test_dict.txt"
+				dict.Build("test_input", test_dict_name)
+				// Start server
+				go func() {
+					listenPort, err := net.Listen("tcp4", fmt.Sprintf(":%d", port))
+					if err != nil {
+						fmt.Println("failed to listen:", err)
+					}
+					registrar = grpc.NewServer()
+					wssi := service.WolperServiceServerImpl{}
+					wssi.Init(test_dict_name)
+					pb.RegisterWolperServiceServer(registrar, &wssi)
+					reflection.Register(registrar)
+					registrar.Serve(listenPort)
+				}()
+
+				// Start client
+				words := service.ClientQuery("localhost:"+strconv.Itoa(port),
+					"", "", "crane")
+				Expect(findWord("crane", words)).To(BeTrue())
+				Expect(findWord("hello", words)).To(BeFalse())
+
+				words = service.ClientQuery("localhost:"+strconv.Itoa(port),
+					"a", "x", ".r..e")
+				Expect(findWord("crane", words)).To(BeTrue())
+			})
+		})
+	})
+
+	Describe("Client and server fails to communicate", func() {
+		Context("With wrong port number", func() {
+			serverPort := 8080
+			clientPort := 8081
+			It("should fail to connect", func() {
+				test_dict_name := "test_dict.txt"
+				dict.Build("test_input", test_dict_name)
+				// Start server
+				go func() {
+					listenPort, err := net.Listen("tcp4", fmt.Sprintf(":%d", serverPort))
+					if err != nil {
+						fmt.Println("failed to listen:", err)
+					}
+					registrar = grpc.NewServer()
+					wssi := service.WolperServiceServerImpl{}
+					wssi.Init(test_dict_name)
+					pb.RegisterWolperServiceServer(registrar, &wssi)
+					reflection.Register(registrar)
+					registrar.Serve(listenPort)
+				}()
+
+				// Start client
+				words := service.ClientQuery("localhost:"+strconv.Itoa(clientPort),
+					"", "", "crane")
+
+				Expect(words).To(BeNil())
+			})
+		})
+	})
+})
