8e53f2b8bdcd5228c84672037341bf932fe4f6e1
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-05-10 19:09:31 +0900

    add test for entropy sort.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

---
+++ dictionary/sorter_test.go
@@ -0,0 +1,67 @@
+package dictionary
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+/*******************************/
+/* Test set up                 */
+/*******************************/
+type SorterSuite struct {
+	suite.Suite
+}
+
+func (suite *SorterSuite) SetupTest() {
+}
+
+/*******************************/
+/* Expose private methods      */
+/*******************************/
+
+/*******************************/
+/* Test cases                  */
+/*******************************/
+func (suite *SorterSuite) TestGetBucketIndex() {
+	target := "abcde"
+	colorPattern := []Color{GREEN, GREEN, GREEN, GREEN, GREEN}
+	suite.Equal(convToBucketIndex(colorPattern), getBucketIndex(target, target))
+	colorPattern = []Color{GREEN, GREEN, GREEN, GREEN, GRAY}
+	suite.Equal(convToBucketIndex(colorPattern), getBucketIndex(target, "abcdf"))
+	colorPattern = []Color{YELLOW, YELLOW, GREEN, GREEN, GRAY}
+	suite.Equal(convToBucketIndex(colorPattern), getBucketIndex(target, "bacdf"))
+	colorPattern = []Color{GREEN, GRAY, GREEN, GREEN, GREEN}
+	suite.Equal(convToBucketIndex(colorPattern), getBucketIndex(target, "aacde"))
+
+	target = "abbcd"
+	colorPattern = []Color{YELLOW, GREEN, YELLOW, GREEN, GREEN}
+	suite.Equal(convToBucketIndex(colorPattern), getBucketIndex(target, "bbacd"))
+	colorPattern = []Color{GRAY, GREEN, GREEN, GREEN, GREEN}
+	suite.Equal(convToBucketIndex(colorPattern), getBucketIndex(target, "bbbcd"))
+
+	target = "bbbcd"
+	colorPattern = []Color{GRAY, GREEN, GREEN, GREEN, GREEN}
+	suite.Equal(convToBucketIndex(colorPattern), getBucketIndex(target, "abbcd"))
+
+	target = "bbbcd"
+	colorPattern = []Color{YELLOW, GRAY, GREEN, YELLOW, GREEN}
+	suite.Equal(convToBucketIndex(colorPattern), getBucketIndex(target, "acbbd"))
+}
+
+func (suite *SorterSuite) TestSortWithEntropy() {
+	words := []string{"aaaaa", "aaaaz", "aaaax", "aaaay", "aaxyz"}
+	sortWithEntropy(words)
+	suite.Equal("aaxyz", words[0])
+	suite.Equal("aaaaa", words[1])
+	suite.Equal("aaaax", words[2])
+	suite.Equal("aaaay", words[3])
+	suite.Equal("aaaaz", words[4])
+}
+
+/*******************************/
+/* Run tests                   */
+/*******************************/
+func TestSorterSuite(t *testing.T) {
+	suite.Run(t, new(SorterSuite))
+}
